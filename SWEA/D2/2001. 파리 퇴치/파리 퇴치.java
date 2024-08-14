db3165934c3c83228df67b2be8566e82ce5ad806
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2024-08-14 15:47:57 +0900

    [D2] Title: 파리 퇴치, Time: 576 ms, Memory: 86,160 KB -BaekjoonHub

---
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	static int T, N, M, maxTotal;
+	static int[][] graph;
+
+	// 이차원 배열로 탐색한다 + cnt로 시작점 (x, y)를 넘겨줘야 한다. -> 최대값 계속 갱신해주기!
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		T = Integer.parseInt(bf.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			maxTotal = 0;
+			graph = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(bf.readLine());
+				for (int j = 0; j < N; j++) {
+					graph[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			check(0, 0);
+			sb.append("#").append(tc).append(" ").append(maxTotal).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static void check(int x, int y) {
+		int total = 0;
+		int[][] map = new int[M][M];
+		int cntX = 0;
+		for (int i = x; i < x + M; i++) {
+			int cntY = 0;
+			for (int j = y; j < y + M; j++) {
+				if (i >= N || j >= N)
+					return;
+				total += graph[i][j];
+				map[cntX][cntY] = graph[i][j];
+				cntY++;
+			}
+			cntX++;
+		}
+		if (maxTotal < total)
+			maxTotal = total;
+		check(x + 1, y);
+		check(x, y + 1);
+
+	}
+}
