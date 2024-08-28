3f556a30d6b8826618a5880b0b2c190fcbed131f
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2024-08-28 17:17:35 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 120 ms, Memory: 19,716 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
@@ -5,37 +5,42 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-
-	static int len, start, size, N = 100;
-	static int maxValue, maxLevel;
-	static boolean[][] adjMatrix;
+	static class Node {
+		int to;
+		Node next;
+		public Node(int to, Node next) {
+			this.to = to;
+			this.next = next;
+		}
+	}
+	
+	static int N = 100, len, M, start;
+	static int maxValue;
+	static Node[] adjList;
 	static boolean[] visited;
 	
 	public static void main(String[] args) throws Exception {
 		StringBuilder sb = new StringBuilder();
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		for(int tc =1; tc <= 10; tc++) {
+		for(int tc = 1; tc <= 10; tc++) {
 			StringTokenizer st = new StringTokenizer(bf.readLine());
 			len = Integer.parseInt(st.nextToken());
 			start = Integer.parseInt(st.nextToken());
-			size = len / 2;
+			M = len / 2;
 			
-			adjMatrix = new boolean[N+1][N+1];
-			visited = new boolean[N+1];
+			adjList = new Node[N + 1];
+			visited = new boolean[N + 1];
 			
 			st = new StringTokenizer(bf.readLine());
-			for(int i = 0; i < size; i++) {
+			for(int i = 0; i < M; i++) {
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
 				
-				adjMatrix[from][to] = true;
+				adjList[from] = new Node(to, adjList[from]);
 			}
 			
-			maxLevel = 1;
 			maxValue = start;
-			
 			bfs(start);
-			
 			sb.append("#").append(tc).append(" ");
 			sb.append(maxValue).append("\n"); 
 		}
@@ -43,29 +48,27 @@ public class Solution {
 	}
 	
 	static void bfs(int start) {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(new int[] {start, 1});
+		Queue<Integer> q = new ArrayDeque<>();
 		visited[start] = true;
+		q.offer(start);
 		
 		while(!q.isEmpty()) {
-			 int[] curr = q.poll();
-			 
-			 int from = curr[0];
-			 int currLevel = curr[1];
-			 
-			 if(maxLevel < currLevel) {
-				 maxLevel = currLevel;
-				 maxValue = from;
-			 } else if(maxLevel == currLevel) {
-				 maxValue = Math.max(maxValue, from);
-			 }
-			 
-			 for(int to = 1; to <= N; to++) {
-				 if(!adjMatrix[from][to]) continue;
-				 if(visited[to]) continue;
-				 visited[to] = true;
-				 q.offer(new int[] {to, currLevel + 1});
-			 }
+			int value = 0;
+			int size = q.size();
+			while(size > 0) {
+				int from = q.poll();
+				value = Math.max(value, from);
+				
+				for(Node n = adjList[from]; n != null; n = n.next) {
+					int to = n.to;
+					if(visited[to]) continue;
+					visited[to] = true;
+					q.offer(to);
+				}
+				
+				maxValue = value;
+				size--;
+			}
 		}
 	}
 }
