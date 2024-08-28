7e29dd5a6a7fe6f709ff3d516b215b26e98de423
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2024-08-28 16:33:49 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 130 ms, Memory: 20,320 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
@@ -5,49 +5,37 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	// 문제의 설명 과정이 BFS 이다.
-	// visited 배열 필요로 함 (이미 연락한 노드엔 더이상 연락을 취하지 않는다)
-	// 큐에 넣어줄 때, 노드의 최대값 갱신 필요
-	
-	static int N = 100, len, start, maxValue, maxLevel;
-	static Node[] adjList;
+
+	static int len, start, size, N = 100;
+	static int maxValue, maxLevel;
+	static boolean[][] adjMatrix;
 	static boolean[] visited;
-	static StringBuilder sb = new StringBuilder();
-	
-	static class Node {
-		int to;
-		Node next;
-		
-		public Node(int to, Node next) {
-			this.to = to;
-			this.next = next;
-		}
-	}
 	
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder();
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
-		for(int tc = 1; tc <= 10; tc++) {
+		for(int tc =1; tc <= 10; tc++) {
 			StringTokenizer st = new StringTokenizer(bf.readLine());
 			len = Integer.parseInt(st.nextToken());
 			start = Integer.parseInt(st.nextToken());
+			size = len / 2;
 			
-			visited = new boolean[N + 1];
-			adjList = new Node[N + 1];
+			adjMatrix = new boolean[N+1][N+1];
+			visited = new boolean[N+1];
 			
 			st = new StringTokenizer(bf.readLine());
-			for(int i = 0; i < len/2; i++) {
+			for(int i = 0; i < size; i++) {
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
 				
-				adjList[from] = new Node(to, adjList[from]);
+				adjMatrix[from][to] = true;
 			}
 			
-			maxValue = start;
 			maxLevel = 1;
+			maxValue = start;
 			
 			bfs(start);
+			
 			sb.append("#").append(tc).append(" ");
 			sb.append(maxValue).append("\n"); 
 		}
@@ -60,26 +48,24 @@ public class Solution {
 		visited[start] = true;
 		
 		while(!q.isEmpty()) {
-			int[] curr = q.poll();
-			
-			int from = curr[0];  	// 출발점
-			int level = curr[1]; 	// bfs 단계
-			
-			if(maxLevel < level) {
-				maxLevel = level;
-				maxValue = from;
-			} else if (maxLevel == level) {
-				maxValue = Math.max(maxValue, from);
-			}
-			
-
-			for(Node n = adjList[from]; n != null; n = n.next) {
-				int to = n.to;
-				if(visited[to]) continue;
-				
-				visited[to] = true;
-				q.offer(new int[] {to, level + 1});
-			}
+			 int[] curr = q.poll();
+			 
+			 int from = curr[0];
+			 int currLevel = curr[1];
+			 
+			 if(maxLevel < currLevel) {
+				 maxLevel = currLevel;
+				 maxValue = from;
+			 } else if(maxLevel == currLevel) {
+				 maxValue = Math.max(maxValue, from);
+			 }
+			 
+			 for(int to = 1; to <= N; to++) {
+				 if(!adjMatrix[from][to]) continue;
+				 if(visited[to]) continue;
+				 visited[to] = true;
+				 q.offer(new int[] {to, currLevel + 1});
+			 }
 		}
 	}
 }
