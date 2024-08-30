952897f9d733c2d373a771deed9b1db8877e6010
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2024-08-30 14:21:15 +0900

    [D4] Title: 최소 스패닝 트리, Time: 3,151 ms, Memory: 193,656 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,97 +1,98 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	
-	static class Edge implements Comparable<Edge>{
-		int start, end, weight;
-
-		public Edge(int start, int end, int weight) {
-			this.start = start;
-			this.end = end;
+	static class Node implements Comparable<Node> {
+		int v;
+		int weight;
+		
+		public Node(int to, int weight) {
+			this.v = to;
 			this.weight = weight;
+		}
+		
+		@Override
+		public int compareTo(Node o) {
+			// TODO Auto-generated method stub
+			return Integer.compare(this.weight, o.weight);
 		}
 
 		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.weight, o.weight);
+		public String toString() {
+			return "[" + v + "," + weight + "]";
 		}
+		
 	}
-	
-	
-	static int T, V, E;
-	static int[] parents;
-	static Edge[] edges;
-	
 	
 	public static void main(String[] args) throws Exception {
 		StringBuilder sb = new StringBuilder();
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		T = Integer.parseInt(bf.readLine());
+		
+		int T = Integer.parseInt(bf.readLine());
 		
 		for(int tc = 1; tc <= T; tc++) {
+			
 			StringTokenizer st = new StringTokenizer(bf.readLine());
+			int V = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
 			
-			V = Integer.parseInt(st.nextToken());
-			E = Integer.parseInt(st.nextToken());
+			List<Node>[] adjList = new List[V+1];
+			for(int i=1; i<=V; i++) adjList[i] = new ArrayList<>();
 			
-			parents = new int[V + 1];
-			edges = new Edge[E];
-			
-			// 간선리스트 받음
-			for(int i = 0; i < E; i++) {
+			boolean[] visited = new boolean[V+1]; // 방문여부 배열 (트리 정점 배열)
+			Queue<Node> q = new PriorityQueue<> ();
+
+			for(int i = 0; i < M; i++) {
 				st = new StringTokenizer(bf.readLine());
-				int start = Integer.parseInt(st.nextToken());
-				int end = Integer.parseInt(st.nextToken());
+				
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
 				int weight = Integer.parseInt(st.nextToken());
 				
-				edges[i] = new Edge(start, end, weight);
+				adjList[from].add(new Node(to,weight));
+				adjList[to].add(new Node(from, weight));
 			}
 			
-			// 각 정점을 단일서로소 집합으로 만들어준다.
-			make();
+			// 정점 1을 시작포인트로 지정
+//			for(int i = 1; i <= V; i++) {
+//				if(adjMatrix[1][i] != 0) q.offer(new Node(1, i, adjMatrix[1][i]));
+//			}
 			
-			// 가중치 기준으로 간선 리스트를 정렬한다.
-			Arrays.sort(edges);
+			q.offer(new Node(1, 0));
 			
-			int cnt = 0;
 			long cost = 0;
-			for(int i = 0; i < E; i++) {
-				if(cnt == V - 1) break;
-				Edge curr = edges[i];
-				if(union(curr.start, curr.end)) {
-					cost += curr.weight;
-					cnt++;
+			int cnt = 0;
+			while(!q.isEmpty()) {
+				// 최소 신장 트리가 완성된 경우 break;
+				// 트리 구성에 포함될 가장 유리한 정점 선택 (비트리 정점 중 최소 비용 간선의 정점 선택)
+				Node curr = q.poll();
+				
+				int min = curr.weight;
+//				int from = curr.from;
+				int from = curr.v;
+
+				if(visited[from]) continue;
+				
+							
+				visited[from] = true;	//
+				cost += (long) min;
+
+				if(++cnt == V) break;
+				
+				// 선택된 정점과 타 정점들 간선 비용 비교하기 (간보기)	
+				for(Node n : adjList[from]) {
+					if(visited[n.v]) continue;
+					q.offer(new Node(n.v, n.weight));
 				}
 			}
 			sb.append("#").append(tc).append(" ").append(cost).append("\n");
 		}
 		System.out.println(sb);
-		
 	}
-	
-	static void make() {
-		for(int i = 1; i <= V; i++) {
-			parents[i] = i;
-		}
-	}
-	
-	static int findSet(int a) {
-		if(parents[a] == a) return a;
-		return parents[a] = findSet(parents[a]);
-	}
-	
-	static boolean union(int a, int b) {
-		int repA = findSet(a);
-		int repB = findSet(b);
-		
-		if(repA == repB) return false;
-		
-		parents[repB] = repA;
-		return true;
-	}
+
 }
