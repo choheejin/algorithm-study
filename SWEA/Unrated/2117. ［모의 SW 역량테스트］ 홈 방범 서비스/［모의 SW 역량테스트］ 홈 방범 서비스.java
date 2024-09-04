65abd06337cabdd5178400086f4e950e7f1757a7
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2024-09-04 17:53:13 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 422 ms, Memory: 87,808 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
@@ -6,12 +6,15 @@ import java.util.StringTokenizer;
 
 public class Solution {
 	
-	// 시간복잡도: 400 * 400 * 20: nullnull하다
+	/*
+	 * 메모리: 115,404 KB, 시간: 1,643 ms, 코드길이: 2,303 Bytes
+	 */
+	
 	// 1. K를 N부터 1까지로 줄이면서 최대 집의 수를 구한다.
 	// 2. 방범서비스를 받을 수 있는 영역은
 	//	  => BFS로 레벨별 탐색: K레벨까지
 	
-	static int N, M;
+	static int N, M, maxHome;
 	static int[][] map;
 	
 	static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
@@ -36,18 +39,11 @@ public class Solution {
 				 }
 			 }
 			 
-			 int maxHome = Integer.MIN_VALUE;
+			 maxHome = Integer.MIN_VALUE;
 			 // step 1
 			 for(int i = 0; i < N; i++) {
 				 for(int j = 0; j < N; j++) {
-					 
-					 for(int k = N+1; k >= 1; k--) {
-						 int home = bfs(i, j, k);
-						 int result = (home * M) - (k * k + (k - 1) * (k - 1));
-						 
-						 if(result >= 0) maxHome = Math.max(maxHome, home);
-					 }
-				 
+					 bfs(i, j);
 				 }
 			 }
 			 sb.append("#").append(tc).append(" ").append(maxHome).append("\n");
@@ -56,7 +52,7 @@ public class Solution {
 		 System.out.println(sb);
 	}
 	
-	static int bfs(int sr, int sc, int k) {
+	static void bfs(int sr, int sc) {
 		boolean[][] visited = new boolean[N][N];
 		Queue<int[]> q = new ArrayDeque<>();
 		
@@ -64,15 +60,18 @@ public class Solution {
 		q.offer(new int[] {sr, sc});
 		
 		int depth = 1;
-		int home = map[sr][sc] == 1 ? 1 : 0;
+		int home = 0;		 
 		
 		while(!q.isEmpty()) {
 			int size = q.size();
-			
-			if(depth == k) break;
-			
+						
 			while(size-- > 0) {
 				int[] curr = q.poll();
+				
+				
+				if(map[curr[0]][curr[1]] == 1) {
+					home++;
+				}
 				
 				for(int i = 0; i < 4; i++) {
 					int nx = curr[0] + dx[i], ny = curr[1] + dy[i];
@@ -80,17 +79,16 @@ public class Solution {
 					if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
 					if(visited[nx][ny]) continue;
 					
-					// 집이 속해있다.
-					if(map[nx][ny] == 1) home++;
-					
 					q.offer(new int[] {nx, ny});
 					visited[nx][ny] = true;
 				}
 				
 			}
+			
+			int result = (home * M) - (depth * depth + (depth - 1) * (depth - 1)); 
+			if(result >= 0) maxHome = Math.max(maxHome, home);
+			
 			depth++;
 		}
-		
-		return home;
 	}
 }
