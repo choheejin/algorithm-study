16bcf946121ac68384fb546271daac4003ec33e5
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2024-10-09 18:01:52 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 6873.85 ms, Memory: 395 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -5,7 +5,7 @@ class Solution {
     static HashMap<String, Integer> combiResult;
     static int max;
     
-    // ** 내가 생각못했던 제약사항: 포함되는 관계는 리턴되지 않나봄: CDE, CD, DE
+    // ** 내가 생각못했던 제약사항은 가장 많이 주문된 횟수라는 것
     // 1. 가능한 조합을 모두 만든다. -> 단, 만들때 중복 없애고 만들자~;
     // 2. 다 만든 조합에 대해 order.contains() 확인하고, hashmap에 저장해준다
     public String[] solution(String[] orders, int[] course) {
@@ -15,21 +15,19 @@ class Solution {
         // combi(0, 0, 3, "");
         int prev = -1;
 
-        for(int i = course.length - 1; i >= 0; i--) {
-            if(prev == course[i]) continue;
+        for(int c: course) {
+            if(prev == c) continue;
             
             max = Integer.MIN_VALUE;
             combiResult = new HashMap<>();
-            combi(0, 0, course[i], "", orders);
+            combi(0, 0, c, "", orders);
             
             for(String key: combiResult.keySet()) {
-                
-                
                 if(combiResult.get(key) != max || combiResult.get(key) < 2) continue;
                 answer.add(key);
             }
                 
-            prev = course[i];
+            prev = c;
         }
         
         Collections.sort(answer);
