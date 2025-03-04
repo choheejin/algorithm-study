4f56ec3443d7fd1f1a92a537c6721107b1589cf1
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2025-03-04 22:36:12 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 76 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
@@ -6,48 +6,31 @@ import java.util.StringTokenizer;
 
 public class Solution {	
 	static StringBuilder sb = new StringBuilder();
+	static int N;
+	static char[] arr;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		for(int tc = 1; tc <= 10; tc++) {
-			int N = Integer.parseInt(bf.readLine());
-
-			char[] dict = new char[N + 1];
-			List<Integer>[] list = new ArrayList[N + 1];
+			N = Integer.parseInt(bf.readLine());
+			arr = new char[N + 1];
 			
 			for(int i = 1; i <= N; i++) {
-				list[i] = new ArrayList<>();
-			}
-			
-			
-			for(int i = 0; i < N; i++) {
-				StringTokenizer st = new StringTokenizer(bf.readLine());	
-			
-				
-				int num = Integer.parseInt(st.nextToken());
-				dict[num] = st.nextToken().charAt(0);
-				
-				while (st.hasMoreTokens()) {
-					list[num].add(Integer.parseInt(st.nextToken()));
-				}
+				arr[i] = bf.readLine().split(" ")[1].charAt(0);
 			}
 			
 			sb.append("#").append(tc).append(" ");
-			inOrderVisit(1, list, dict);
+			inOrder(1);
 			sb.append("\n");
 		}
 		System.out.println(sb);
 	}
 	
-	public static void inOrderVisit(int node, List[] list, char[] dict) {
-		if(list[node].size() >= 1) {
-			inOrderVisit((int) list[node].get(0), list, dict);
-		}
+	public static void inOrder(int node) {
+		if(node > N) return;
 		
-		sb.append(dict[node]);
-		
-		if(list[node].size() >= 2) {
-			inOrderVisit((int) list[node].get(1), list, dict);
-		}
+		inOrder(node * 2);
+		sb.append(arr[node]);
+		inOrder(node * 2 + 1);
 	}
 }
