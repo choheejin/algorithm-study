849a578886edce1bf2d81f2a28e8114aeab4f1fa
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2025-03-17 21:11:48 +0900

    [Silver III] Title: 1로 만들기, Time: 104 ms, Memory: 15820 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,27 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-
-		int N = sc.nextInt();
-		
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(bf.readLine());
 		int[] dp = new int[1000001];
 		
 		dp[2] = 1;
 		dp[3] = 1;
 		
 		for(int i = 4; i <= N; i++) {
-			if(i % 3 == 0 && i % 2 == 0) {
-				dp[i] = Math.min(dp[i/3], Math.min(dp[i/2], dp[i-1])) + 1;
+			if(i % 2 == 0 && i % 3 == 0) {
+				dp[i] = Math.min(dp[i / 3] + 1, dp[i / 2] + 1);
+				dp[i] = Math.min(dp[i], dp[i-1] + 1);
 			}
 			else if(i % 3 == 0) {
-				dp[i] = Math.min(dp[i/3], dp[i-1]) + 1;
+				dp[i] = dp[i / 3] + 1;
+				dp[i] = Math.min(dp[i], dp[i-1] + 1);
 			}
 			else if(i % 2 == 0) {
-				dp[i] = Math.min(dp[i/2], dp[i-1]) + 1;
+				dp[i] = dp[i / 2] + 1;
+				dp[i] = Math.min(dp[i], dp[i-1] + 1);
 			}
 			else {
 				dp[i] = dp[i-1] + 1;
