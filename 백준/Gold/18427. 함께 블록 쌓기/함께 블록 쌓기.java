b01c3b70b5dae7ac87e3e28f92434b7d8bdab7a2
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2025-03-28 23:26:53 +0900

    [Gold IV] Title: 함께 블록 쌓기, Time: 212 ms, Memory: 19076 KB -BaekjoonHub

--- 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.java
+++ 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.java
@@ -14,27 +14,25 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		H = Integer.parseInt(st.nextToken());
 		
-		int[][] dp = new int[N + 1][H + 1];
-		
-		dp[0][0] = 1;
+		int[] dp = new int[H+1];
+				
+		dp[0] = 1;
 		
 		for(int i = 1; i <= N; i++) {
 			int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-			
-			System.arraycopy(dp[i-1], 0, dp[i], 0, H + 1);
+			int[] next = new int[H+1];
+			System.arraycopy(dp, 0, next, 0, H + 1);
 
 			for(int num : nums) {
 				for(int j = 0; j <= H - num; j++) {
-					if(dp[i - 1][j] >= 1) {
-						dp[i][j + num] = (dp[i][j + num] + dp[i - 1][j]) % MOD;
-					}
+					next[j + num] = (next[j + num] + dp[j]) % MOD;
 				}
 			}
 			
-//			System.out.println(Arrays.toString(dp[i]));
+			dp = next;
 		}
 		
-		System.out.println(dp[N][H]);
+		System.out.println(dp[H]);
 	}
 	
 }
