676f75153f64bf825a1d77b7bd505f8897fb4316
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2025-04-25 15:36:19 +0900

    [Gold V] Title: 평범한 배낭, Time: 100 ms, Memory: 12272 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,37 +1,43 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		int[][] dp = new int[N + 1][K + 1];
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
 
-		int[][] products = new int[N + 1][2];
-		
-		for(int i = 1; i <= N; i++) {
-			st = new StringTokenizer(bf.readLine());
-			products[i][0] = Integer.parseInt(st.nextToken());
-			products[i][1] = Integer.parseInt(st.nextToken());
-		}
-		
-		for(int i = 1; i <= K; i++) {
-			for(int n = 1; n <= N; n++) {
-				if(products[n][0] <= i) {
-					dp[n][i] = Math.max(dp[n-1][i],dp[n-1][i - products[n][0]] + products[n][1]);
-				} else {
-					dp[n][i] = dp[n- 1][i];
-				}
-			}
-		}
-		
-		System.out.println(dp[N][K]);
-	}
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] weight = new int[N];
+        int[] values = new int[N];
+
+        int maxValue = 0;
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(bf.readLine());
+
+            weight[i] = Integer.parseInt(st.nextToken());
+            values[i] = Integer.parseInt(st.nextToken());
+            maxValue += values[i];
+        }
+
+        int INF = 100_000_000;
+        int[] dp = new int[maxValue + 1];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+        for(int i = 0; i < N; i++) {
+            for(int j = maxValue; j >= values[i]; j--) {
+                dp[j] = Math.min(dp[j], dp[j - values[i]] + weight[i]);
+            }
+        }
+
+        for(int i = maxValue; i >= 0; i--) {
+            if(dp[i] <= K) {
+                System.out.println(i);
+                return;
+            }
+        }
+    }
 }
