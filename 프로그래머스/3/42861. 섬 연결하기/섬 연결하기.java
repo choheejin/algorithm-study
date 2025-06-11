5551adc760bf0aef146269091c1abdb3db09de99
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2025-06-11 21:11:16 +0900

    [level 3] Title: 섬 연결하기, Time: 4.13 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,12 +1,53 @@
-// 최소신장트리임
 import java.util.*;
 
 class Solution {
-    static List<int[]>[] adjList;
+    static class Node {
+        int to, cost;
+        
+        public Node (int to, int cost) {
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+    
+    static List<Node>[] adjList;
     
     public int solution(int n, int[][] costs) {
-        int answer = 0;
+        init(n, costs);
         
+        int answer = prim(n);
+
+        return answer;
+    }
+    
+    public int prim (int N) {
+        boolean[] visited = new boolean[N];        
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+        
+        int minCost = 0;
+        
+        q.offer(new int[] {0, 0});
+        
+        while(!q.isEmpty()) {
+            int[] currNode = q.poll();
+            
+            int from = currNode[0];
+            int cost = currNode[1];
+            
+            if(visited[from]) continue;
+            visited[from] = true;
+            minCost += cost;
+            
+            for(Node n : adjList[from]) {
+                if(visited[n.to]) continue;
+                q.offer(new int[] {n.to, n.cost});
+            }
+        }
+        
+        return minCost;
+    }
+    
+    public static void init (int n, int[][] costs) {
         adjList = new ArrayList[n];
         
         for(int i = 0; i < n; i++) {
@@ -14,39 +55,12 @@ class Solution {
         }
         
         for(int i = 0; i < costs.length; i++) {
-            adjList[costs[i][0]].add(new int[] {costs[i][1], costs[i][2]});
-            adjList[costs[i][1]].add(new int[] {costs[i][0], costs[i][2]});
+            int from = costs[i][0];
+            int to = costs[i][1];
+            int cost = costs[i][2];
+            
+            adjList[from].add(new Node(to, cost));
+            adjList[to].add(new Node(from, cost));
         }
-        
-        answer = prim(costs, n);
-        
-        return answer;
     }
-    
-    public static int prim(int[][] costs, int n) {
-        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        boolean[] visited = new boolean[n];
-        int cost = 0;
-        
-        // 1. 첫 방문 노드 선정
-        q.offer(new int[] {0, 0});
-        
-        while(!q.isEmpty()) {
-            int[] curr = q.poll();
-            
-            if(visited[curr[0]]) continue;
-            
-            cost += curr[1];
-            
-            // 방문하지 않은 인접 노드들을 기준으로 최소비용 구한다~
-            for(int i = 0; i < adjList[curr[0]].size(); i++) {
-                if(visited[adjList[curr[0]].get(i)[0]]) continue;
-                q.offer(adjList[curr[0]].get(i));
-            }
-            visited[curr[0]] = true;
-        }
-        return cost;
-
-    }
-    
 }
