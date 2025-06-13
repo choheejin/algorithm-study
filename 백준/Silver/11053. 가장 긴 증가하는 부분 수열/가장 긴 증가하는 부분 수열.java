db4ea8495364527f70d87e1ddaed87bef34fc002
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2025-06-13 17:33:57 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 68 ms, Memory: 11784 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,42 +1,49 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(bf.readLine());
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-
-        int[] dp = new int[N+1];
-        int end = 0;
-        for(int i = 1; i <= N; i++) {
-            int target = Integer.parseInt(st.nextToken());
-            if(dp[end] < target) {
-                dp[++end] = target;
-            }
-            else {
-                int idx = binary_search(dp, end, target);
-                dp[idx] = target;
-            }
-        }
-
-        System.out.println(end);
-    }
-
-    private static int binary_search(int[] arr, int end, int target) {
-        int start = 1;
-        while(start < end) {
-            int mid = (start + end) / 2;
-            if(arr[mid] >= target) {
-                 end = mid;
-                 continue;
-            }
-
-            start = mid + 1;
-        }
-        return end;
-    }
+	static int[] dp;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf =new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(bf.readLine());		
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		int[] nums = new int[N];
+		dp = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		dp[0] = nums[0];
+		int end_idx = 0;
+		for(int i = 1; i < N; i++) {
+			if(dp[end_idx] < nums[i]) {
+				dp[++end_idx] = nums[i];
+			} else {
+				int idx = binary_search(end_idx, nums[i]);
+				dp[idx] = nums[i];
+			}
+		}
+		System.out.println(end_idx + 1);
+	}
+	
+	public static int binary_search(int end, int target) {
+		int start = 0;
+		while (start < end) {
+			int mid = (start + end) / 2;
+			
+			if(dp[mid] >= target) {
+				end = mid;
+				continue;
+			}
+			
+			start = mid + 1;
+		}
+		
+		return end;
+	}
 }
