7ae8127906aa8e3c3cd923c01aeb28d81c4ec7ae
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2025-06-13 16:49:47 +0900

    [Silver I] Title: 스티커, Time: 600 ms, Memory: 125828 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -3,37 +3,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(bf.readLine());
+		
+		for(int tc = 0; tc < T; tc++) {
+			int N = Integer.parseInt(bf.readLine());
+			
+			int[][] nums = new int[2][N+1];
+			long[][] dp = new long[2][N+1];
+			
+			StringTokenizer st1 = new StringTokenizer(bf.readLine());
+			StringTokenizer st2 = new StringTokenizer(bf.readLine());
+			
+			for(int j = 1; j <= N; j++) {
+				nums[0][j] = Integer.parseInt(st1.nextToken());
+				nums[1][j] = Integer.parseInt(st2.nextToken());
+			}
 
-        int T = Integer.parseInt(bf.readLine());
-
-        for(int tc = 1; tc<= T; tc++) {
-            int N = Integer.parseInt(bf.readLine());
-
-            int[][] nums = new int[N+1][2];
-            int[][] dp = new int[N+1][2];
-
-            StringTokenizer st1 = new StringTokenizer(bf.readLine());
-            StringTokenizer st2 = new StringTokenizer(bf.readLine());
-
-            for(int i = 1; i <= N; i++) {
-                nums[i][0] = Integer.parseInt(st1.nextToken());
-                nums[i][1] = Integer.parseInt(st2.nextToken());
-            }
-
-            dp[0][0] = dp[0][1] = 0;
-            dp[1][0] = nums[1][0]; dp[1][1] = nums[1][1];
-
-            for(int i = 2; i <= N; i++) {
-                dp[i][0] = Math.max(dp[i-1][1], dp[i-2][1]) + nums[i][0];
-                dp[i][1] = Math.max(dp[i-1][0], dp[i-2][0]) + nums[i][1];
-            }
-
-            int result = Math.max(dp[N][0], dp[N][1]);
-            sb.append(result).append("\n");
-        }
-        System.out.print(sb);
-    }
+			dp[0][1] = nums[0][1];
+			dp[1][1] = nums[1][1];
+			
+			for(int i = 2; i <= N; i++) {
+				long preMax = Math.max(dp[0][i-2], dp[1][i-2]);
+				
+				dp[0][i] = Math.max(dp[1][i-1], preMax) + nums[0][i];
+				dp[1][i] = Math.max(dp[0][i-1], preMax) + nums[1][i];
+ 			}
+			
+			
+			long maxValue = Math.max(dp[0][N], dp[1][N]);
+			sb.append(maxValue).append("\n");
+		}
+		System.out.println(sb);
+	}
 }
