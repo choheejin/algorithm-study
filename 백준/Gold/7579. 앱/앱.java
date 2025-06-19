46437394a4d19946826b8f0348bd94d26b7bf640
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2025-06-19 15:06:08 +0900

    [Gold III] Title: 앱, Time: 84 ms, Memory: 11976 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -1,42 +1,42 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-	
 		StringTokenizer st = new StringTokenizer(bf.readLine());
-		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		int maxCost = 0;
+		int maxCosts = 0;
 		int[] memories = new int[N];
-		int[] costs = new int[N];
+		int[] swapCosts = new int[N];
 		
 		StringTokenizer st1 = new StringTokenizer(bf.readLine());
 		StringTokenizer st2 = new StringTokenizer(bf.readLine());
 		
 		for(int i = 0; i < N; i++) {
 			memories[i] = Integer.parseInt(st1.nextToken());
-			costs[i] = Integer.parseInt(st2.nextToken());
-			maxCost += costs[i];
+			swapCosts[i] = Integer.parseInt(st2.nextToken());
+			maxCosts += swapCosts[i];
 		}
 		
-		int[] dp = new int[maxCost + 1];
+		int[] dp = new int[maxCosts + 1];
 		
 		for(int i = 0; i < N; i++) {
-			for(int j = maxCost; j >= costs[i]; j--) {
-				dp[j] = Math.max(dp[j], dp[j - costs[i]] + memories[i]);
+			for(int m = maxCosts; m >= swapCosts[i]; m-- ) {
+				dp[m] = Math.max(dp[m], dp[m - swapCosts[i]] + memories[i]);
 			}
 		}
 		
-		for(int i = 0; i <= maxCost; i++) {
+		for(int i = 0; i <= maxCosts; i++) {
 			if(dp[i] >= M) {
 				System.out.println(i);
 				return;
 			}
 		}
+		
 	}
 }
